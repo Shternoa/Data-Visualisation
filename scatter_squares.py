2fc75a587a3daa2813df8b671dcfc0442b5cbe23
Elman <134933773+Shternoa@users.noreply.github.com>
2023-06-08 15:15:26 +0300

    Цветовые карты

--- scatter_squares.py
+++ scatter_squares.py
@@ -2,7 +2,8 @@ import matplotlib.pyplot as plt
 
 x_points = list(range(1, 1001))
 y_points = [x ** 2 for x in x_points]
-plt.scatter(x_points, y_points, c='purple', edgecolors='none', s=50)
+colors = list(range(len(x_points)))
+plt.scatter(x_points, y_points, c=colors, cmap=plt.cm.Blues, edgecolors='none', s=50)
 
 plt.title("Square numbers", fontsize=24)
 plt.xlabel("Value", fontsize=14)
