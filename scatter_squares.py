bdf120324d196c5425d0424122bd53b3e2966820
Elman <134933773+Shternoa@users.noreply.github.com>
2023-06-08 14:58:04 +0300

    Автоматическое вычесление данных

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 
-x_points = [1, 2, 3, 4, 5]
-y_points = [2, 4, 9, 16, 26]
-plt.scatter(x_points, y_points, s=50)
+x_points = list(range(1, 1001))
+y_points = [x ** 2 for x in x_points]
+plt.scatter(x_points, y_points, c='purple', edgecolors='none', s=50)
 
 plt.title("Square numbers", fontsize=24)
 plt.xlabel("Value", fontsize=14)
 plt.ylabel("Square Value", fontsize=14)
 plt.tick_params(axis='both', labelsize=14)
+plt.axis([0, 1100, 0, 1100000])
 plt.show()
