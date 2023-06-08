ea0cbee1e780eacdec18a6ffc1eef20f9963b148
Elman <134933773+Shternoa@users.noreply.github.com>
2023-06-08 14:38:29 +0300

    Изменение надписей и толщины графика

--- mpl_suares.py
+++ mpl_suares.py
@@ -1,5 +1,11 @@
 import matplotlib.pyplot as plt
 
 squares = [1, 4, 9, 16, 25]
-plt.plot(squares)
+plt.plot(squares, linewidth=5)
+
+# Заголовок программы и метка осей
+plt.title("Square numbers", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square Value", fontsize=14)
+plt.tick_params(axis='both', labelsize=14)
 plt.show()
