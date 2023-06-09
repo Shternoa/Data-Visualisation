485015af5af50d4db1102b5ad59c8fa15984311f
Elman <134933773+Shternoa@users.noreply.github.com>
2023-06-09 14:03:54 +0300

    Изменение на молекулярное движение

--- random_walk_visual.py
+++ random_walk_visual.py
@@ -6,16 +6,16 @@ from random_walk import RandomWalk
 
 while True:
     # Построенеи случайного блуждания с нанесением точек на диаграмму
-    rw = RandomWalk(50000)
+    rw = RandomWalk(5000)
     rw.fill_walk()
     # Область просмотра
     plt.figure(dpi=128, figsize=(10, 6))
     point_numbers_list = list(range(rw.num_points))
-    plt.scatter(rw.x_point, rw.y_point, c=point_numbers_list, cmap=plt.cm.Greens, edgecolors='none', s=1)
+    plt.plot(rw.x_point, rw.y_point, c='purple', linewidth=3)
 
     # Выделение первой и последней точек.
-    plt.scatter(0, 0, c='red', edgecolors='none', s=50)
-    plt.scatter(rw.x_point[-1], rw.y_point[-1], c='blue', edgecolors='none', s=50)
+    plt.scatter(0, 0, c='red', linewidth=30)
+    plt.scatter(rw.x_point[-1], rw.y_point[-1], c='blue', linewidth=30)
 
     # Удаление осей
     # plt.axes().get_xaxis().set_visible(True)
