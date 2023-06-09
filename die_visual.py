ef9e12ab6b8ac973147d011559a08225f5c51836
Elman <134933773+Shternoa@users.noreply.github.com>
2023-06-09 14:56:28 +0300

    Анализ результатов и посторение гистограммы

--- die_visual.py
+++ die_visual.py
@@ -1,11 +1,26 @@
 from die import Die
-
+import pygal
 
 die = Die()
 
 # Моделировние серии бросков
 results = []
-for roll_number in range(100):
+for roll_number in range(1000):
     result = die.roll()
     results.append(result)
-print(results)
+# print(results)
+
+frequencies = []
+for value in range(1, die.num_sides + 1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+# print(frequencies)
+
+hist = pygal.Bar()
+
+hist.title = 'Результаты броска кубика 1000 раз'
+hist.x_labels = ['1', '2', '3', '4', '5', '6']
+hist.x_title = 'Результат'
+hist.y_title = 'Частота результата'
+hist.add('Сторона кубика №', frequencies)
+hist.render_to_file('die_visual.svg')
