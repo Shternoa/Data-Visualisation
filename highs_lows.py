6fefe0c2b3a3e21e228e3932269f5c56ba92a6a8
Elman <134933773+Shternoa@users.noreply.github.com>
2023-06-13 10:55:20 +0300

    Цветовое выделение и проверка ошибок

--- highs_lows.py
+++ highs_lows.py
@@ -4,20 +4,26 @@ from matplotlib import pyplot as plt
 from datetime import datetime
 
 # Чтение дат и максимальных температур из файла
-file_name = 'sitka_weather_2014.csv'
+file_name = 'death_valley_2014.csv'
 with open(file_name) as file:
     reader = csv.reader(file)
     header_row = next(reader)
+    # for index, column_reader in enumerate(header_row):
+    #     print(index, column_reader)
     dates, highs, lows = [], [], []
     for row in reader:
-        current_date = datetime.strptime(row[0], '%Y-%m-%d')
-        dates.append(current_date)
-        high = int(row[1])
-        h_c = (high - 32) * 5 / 9
-        highs.append(round(h_c))
-        low = int(row[3])
-        l_c = (low - 32) * 5 / 9
-        lows.append(round(l_c))
+        try:
+            current_date = datetime.strptime(row[1], '%Y-%m-%d')
+            high = int(row[2])
+            low = int(row[4])
+            l_c = (low - 32) * 5 / 9
+            h_c = (high - 32) * 5 / 9
+        except ValueError:
+            print(current_date, 'missing_data')
+        else:
+            dates.append(current_date)
+            highs.append(round(h_c))
+            lows.append(round(l_c))
     print(highs)
     print(lows)
 # first_date = datetime.strptime('2014-7-1', '%Y-%m-%d')
@@ -25,11 +31,12 @@ with open(file_name) as file:
 
 # Нанесение данных на диаграмму
 fig = plt.figure(dpi=128, figsize=(10, 6))
-plt.plot(dates, highs, c='green')
-plt.plot(dates, lows, c='red')
+plt.plot(dates, highs, c='green', alpha=0.5)
+plt.plot(dates, lows, c='red', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='yellow', alpha=0.1)
 
 # Форматирование диаграммы
-plt.title('Температуры по дням за 2014', fontsize=24)
+plt.title('Температуры по дням за 2014 в Долине Смерти', fontsize=24)
 plt.xlabel('', fontsize=12)
 fig.autofmt_xdate()
 plt.ylabel('Температура (C)', fontsize=12)
