9c76109363c8ab1aa6406aa4b0b5f8f71c79f9b3
Elman <134933773+Shternoa@users.noreply.github.com>
2023-06-14 10:18:19 +0300

    Построение полной карты населения

--- americans.py
+++ americans.py
@@ -4,7 +4,8 @@ from pygal_maps_world.maps import World
 wm = World()
 wm.title = ('North, Central, and South America')
 
-wm.add('North America', ['ca', 'mx', 'us'])
+wm.add('North America', {'ca': 34126000, 'us': 309349000, 'mx':
+    113423000})
 wm.add('Central America', ['bz', 'cr', 'gt', 'hn', 'ni', 'pa', 'sv'])
 wm.add('South America', ['ar', 'bo', 'br', 'cl', 'co', 'ec', 'gf',
                          'gy', 'pe', 'py', 'sr', 'uy', 've'])
--- world_population.py
+++ world_population.py
@@ -1,6 +1,7 @@
 import json
 from pygal_maps_world.i18n import COUNTRIES
 from countries_code import get_country_code
+from pygal_maps_world.maps import World
 
 file_name = 'population_data.json'
 
@@ -9,15 +10,20 @@ with open(file_name) as file:
 
 # Вывод население страны за 2010 год
 
+ww_population = {}
 for pop_dict in pop_data:
     if pop_dict['Year'] == '2010':
         country_name = pop_dict['Country Name']
-        population = pop_dict['Value']
+        population = float(pop_dict['Value'])
         code = get_country_code(country_name)
         if code:
-            print(f'{country_name} : {population}')
-        else:
-            print(f'Error : {str(population)}')
+            ww_population[code] = population
+        # else:
+        #     print(f'Error : {str(population)}')
+wm = World()
+wm.title = 'World Population in 2010, by Country'
+wm.add('2010', ww_population)
+wm.render_to_file('world_population.svg')
 
 # for country_code in sorted(COUNTRIES.keys()):
 #     print(country_code, COUNTRIES[country_code])
